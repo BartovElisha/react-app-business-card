38e0529f436f5082808255b49f42c8e4cc96ee42
ElishaBartov <elisha.bartov@emerson.com>
2023-01-06 18:14:14 +0200

    Added API Service.ts

---
+++ src/services/apiService.ts
@@ -0,0 +1,66 @@
+import { getToken, verifyToken } from "../auth/tokenMenagment";
+
+const serverUrl = 'http://lockalhost:3000/';
+
+const handleRequest = (
+    url: string,
+    method: string,
+    headers?: HeadersInit,
+    data?: object,
+    checkToken = true): Promise<Response> | null => {
+        if (checkToken && !verifyToken()) {
+            return null;
+        }
+
+        const config = {
+            method,
+            headers: {
+                ...headers,
+                'x-auth-token' : getToken()
+            },
+            body: (data) ? JSON.stringify(data) : null
+        }
+
+    return fetch(url, config);
+}
+
+export const getRequest = (endpoint: string): Promise<Response> | null => {
+    return handleRequest(
+        `${serverUrl}${endpoint}`,
+        'GET'
+    );
+}
+
+export const postRequest = (
+    endpoint: string, 
+    data: object,
+    checkToken?: boolean): Promise<Response> | null => {
+    return handleRequest(
+        `${serverUrl}${endpoint}`,
+        'POST',
+        {'Content-Type': 'application/json'},
+        data,
+        checkToken
+    ); 
+}
+
+export const patchRequest = (
+    endpoint: string, 
+    data: object): Promise<Response> | null => {
+    return handleRequest(
+        `${serverUrl}${endpoint}`,
+        'PATCH',
+        {'Content-Type': 'application/json'},
+        data
+    ); 
+}
+
+export const deleteRequest = (
+    endpoint: string): Promise<Response> | null => {
+    return handleRequest(
+        `${serverUrl}${endpoint}`,
+        'DELETE'
+    ); 
+}
+
+
