b4bd116bf9d450863fe2c22af18dd8556f08a0b5
ElishaBartov <elisha.bartov@emerson.com>
2023-01-07 11:48:43 +0200

    Added Buisness user registration.

--- src/auth/BusinessUserRegistration.tsx
+++ src/auth/BusinessUserRegistration.tsx
@@ -1,39 +1,38 @@
 import Joi from "joi";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import Title from "../components/Title";
-
+import { postRequest } from "../services/apiService";
 
 interface IBusinessUserData {
-    firstName: string;
-    lastName: string;
+    name: string
     email: string;
     password: string;
-    biz: boolean;
+    izBiz: boolean;
 }
 
 function BusinessUserRegistration() {
     // States
     const navigate = useNavigate();
-    const [firstName, setFirstName] = useState<string>('');
-    const [lastName, setLastName] = useState<string>('');
+    const [name, setName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
     const [error, setError] = useState<string>('');
 
     function submit() {
         const schema = Joi.object().keys({
-            firstName: Joi.string().required().min(2).max(255),
-            lastName: Joi.string().required().min(2).max(255),
+            name: Joi.string().required().min(2).max(255),
             email: Joi.string().required().min(6).max(255).email({ tlds: { allow: false}}),
-            password: Joi.string().required().min(6).max(30)
+            password: Joi.string().required().min(6).max(30),
+            isBiz: Joi.boolean().required()
         });
 
         const { error, value } = schema.validate({
-            firstName,
-            lastName,
+            name,
             email,
-            password
+            password,
+            isBiz: true
         });
 
         if (error) {
@@ -46,10 +45,44 @@ function BusinessUserRegistration() {
     }
 
     function register(data: IBusinessUserData) {
-        // 1. Send data to the server Rest API...
-        console.log(data);
-        // 2. If All OK Navigate to Signin
-        navigate('/businesscardregistration');
+        const res = postRequest(
+            'users/signup',
+            data,
+            false
+        );
+
+        if (!res) {
+            return;
+        } 
+
+        res
+        .then(response => response.json())
+        .then(json => {
+            if (json.error) {
+                toast.error(json.error, {
+                    position: "top-center",
+                    autoClose: 3000,
+                    hideProgressBar: true,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });                   
+                return;
+            }
+            toast.success(`New Business User ${json.name} Added succsessifully`,{
+                position: "top-center",
+                autoClose: 3000,
+                hideProgressBar: true,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored",
+            });              
+            navigate('/businesscardregistration');
+        });        
     }
 
     return ( 
@@ -66,19 +99,8 @@ function BusinessUserRegistration() {
                         type="text"
                         className="form-control text-muted mb-3"
                         placeholder="First Name"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                    >
-                    </input>
-                </div>
-                <div className="mp-3">
-                    <label className="mb-2 fs-5">Last Name:</label>
-                    <input
-                        type="text"
-                        className="form-control text-muted mb-3"
-                        placeholder="Last Name"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
                     >
                     </input>
                 </div>
