ecb0d53721e67e1edfdd340265c03299f16fd2ef
ElishaBartov <elisha.bartov@emerson.com>
2023-01-26 23:30:16 +0200

    Starder design Menu search Bar.

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,11 +1,27 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../App";
 
 function MenuBar() {
     const context = useContext(AppContext);
+    // States
+    const [searchValue, setSearch] = useState<string>('');
     
     if (!context) {
         return <div>Error</div>
+    }
+
+    function handleSearch(value: string) {
+        // 1. Update form input Value
+        setSearch(value);
+        console.log(value);
+
+        // 2. Convert value to lowerCase
+        let term = searchValue.toLowerCase();
+
+        // 3. copy businessCards to result array
+
+        // 4. Filter result by term
+
     }
 
     return (  
@@ -24,6 +40,8 @@ function MenuBar() {
                     <i className="bi-list-task"></i>
                 </button>
                 <input
+                    onChange={(e) => handleSearch(e.target.value)}
+                    type="text"
                     className="form-control ms-3"
                     placeholder="Enter business name or number"
                 ></input>
