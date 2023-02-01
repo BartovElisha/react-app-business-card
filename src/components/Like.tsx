5c0cca50b47ef34c7e79ead4f03ad9b7d36e9ef5
ElishaBartov <elisha.bartov@emerson.com>
2023-02-02 01:08:51 +0200

    Fixed Like component.

--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -16,10 +16,10 @@ function Like({ card_id }: Props) {
         return <div>Error</div>;
     }   
 
-    function updateBusinessCardLikesList() {
+    function getUsersLikesList() {
         console.log(`User ${context?.userName} added like to the card ID: ${card_id}`); 
 
-        // 1. Read business card data from database
+        // Read business card data from database
         const res = getRequest(`cards/${card_id}`);
         if (!res) {
             return;
@@ -33,30 +33,16 @@ function Like({ card_id }: Props) {
                 return;
             }
             setUsersLikesList(json.users_likes_id);
-            // I'am here ...
-            // if need add like , need push user_is to array and send to database, 
-            // if need remove like, need find user id in array, remove it and send to database.
         });
-
-        // 2. Update users likes list array
-        console.log(usersLikesList);
-
-        // 3. Update business card database
-        // const res = patchRequest(
-        //     `cards/${businessCard._id}`,
-        //     data
-        // );
-        // if (!res) {
-        //     return;
-        // }
-        
-        // res
-        // .then(response => response.json())
-        
     }        
 
+    function updateLikesList() {
+        console.log(usersLikesList);
+    }
+
     function likeToggle() {
-        updateBusinessCardLikesList();
+        getUsersLikesList();
+        updateLikesList();
         setLike(!like);    
     }    
 
